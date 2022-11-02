489be7496b0123340e97db49097a40aa4ac6ced9
chiragrajbr <115540327+chiragrajbr@users.noreply.github.com>
2022-11-02 10:46:26 +0530

    function overriding

---
+++ function/fatarrow1.js
@@ -0,0 +1,10 @@
+//fat arrow function does not override
+var a=()=>{
+    console.log("good morning")
+}
+a()
+
+var a=()=>{
+    console.log("good night")
+}
+a()
---
+++ function/function1.js
@@ -0,0 +1,15 @@
+//function that overrides
+function print(){
+    console.log("have a plesant day")
+}
+print()
+
+function print(){
+    console.log("have a plesant morning")
+}
+print()
+
+//output=have a plesant morning
+//       have a plesant morning
+
+
