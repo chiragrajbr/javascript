df1376041106a1a20fda9797b2d8429d6a640ac0
chiragrajbr <115540327+chiragrajbr@users.noreply.github.com>
2022-11-10 10:47:37 +0530

    export import

---
+++ importexport/export.js
@@ -0,0 +1,12 @@
+//exporting variable,function and class
+let a=10
+function instructions(){
+     console.log("you should be in time ")
+}
+class bonus{
+    constructor(name){
+        this.name=name
+    }
+
+}
+module.exports={a,instructions,bonus}
---
+++ importexport/import.js
@@ -0,0 +1,6 @@
+const {a,instructions,bonus}=require("./export")
+console.log(a)
+console.log(instructions())
+let b= new bonus("Chirag")
+
+console.log(b)
