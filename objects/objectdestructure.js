5e5770781bd146427e9fa0534e89aded619931e0
chiragrajbr <115540327+chiragrajbr@users.noreply.github.com>
2022-11-23 19:42:10 +0530

    object destructuring

---
+++ objects/objectdestructure.js
@@ -0,0 +1,11 @@
+let details={
+    name:"chirag",
+    age:21,
+    email:"chiragrajbr@gmail.com",
+    father_name:"raju bn",
+    mother_name:"bharathi mp"
+}
+//console.log(details.name)
+//object destructure-- taking some values from object and stroing them in const,var,let
+const{name,age}=details
+console.log(name,age)
