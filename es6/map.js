959e9bca4cbb11afd95bb4f70e749db98e9dddde
chiragrajbr <115540327+chiragrajbr@users.noreply.github.com>
2022-11-30 11:15:38 +0530

    map method

---
+++ es6/map.js
@@ -0,0 +1,14 @@
+//map method-it itterates every elements in array and execute the provided function
+//we can pass 3 arguments
+/*1--it iterates the provided function
+  2--it indicates the index and display required index
+  3--it indicates the whole array and display whole array  */
+
+
+  var names=[{id:101,name:"ccc",sal:1254},{id:102,name:"ccc",sal:125}]
+names.map((k)=>{
+    console.log(k.id)
+
+})
+
+
